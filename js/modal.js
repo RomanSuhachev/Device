56c46555d5258375f356399bc1746fba2f726ac8
RomanDrakon <psionik@bk.ru>
2022-11-10 23:38:52 +0300

    добавил каталог товаров и оформление, сетку

--- js/modal.js
+++ js/modal.js
@@ -10,3 +10,18 @@ modalBtn.addEventListener('click', (e) => {
 closeModalBtn.addEventListener('click', () => {
   modal.style.display = "none";
 })
+
+
+function radioChecked() {
+  const radio = document.querySelectorAll(".controls-radio input");
+  const sliderQuaranty = document.querySelector(".slider-quaranty");
+
+  radio.forEach(item => {
+    item.classList.remove(".active-slider");
+
+    if(item.checked.classList.contains("#quaranty")) {
+      sliderQuaranty.classList.add("active-slider")
+    }})
+  }
+
+  document.addEventListener("DOMContentLoaded", radioChecked)
